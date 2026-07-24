c849444109a188f316ea8068be20bd8f26ee48cc
EugenePrygaro <eugeneprygaro@gmail.com>
2026-07-24 23:49:18 +0300

    added header and home page

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import css from "./Header.module.css";
+import { usePathname } from "next/navigation";
+
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <header className="bg-blue-600 text-white p-4">
-      <h1 className="text-2xl font-bold">Travel Trucks</h1>
+    <header className={css.header}>
+      <div className={css["header-content"]}>
+        <Link href="/" className={css.logo}>
+          <Image
+            src="/Icons/TravelTrucks.svg"
+            alt="Logo"
+            width={136}
+            height={16}
+            className={css.icon}
+          />
+        </Link>
+        <div className={css["header-links"]}>
+          <Link
+            href="/"
+            className={`${css.link} ${pathname === "/" ? css.active : ""}`}
+          >
+            Home
+          </Link>
+          <Link
+            href="/catalog"
+            className={`${css.link} ${pathname === "/catalog" ? css.active : ""}`}
+          >
+            Catalog
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
