979866665de9040827804d239acf81c30f6b4eaf
EugenePrygaro <eugeneprygaro@gmail.com>
2026-07-27 15:32:20 +0300

    Add mobile adaptive styles for catalog, list, details and gallery

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useState, useRef } from "react";
 import CamperList from "@/components/CamperList/CamperList";
 import FiltersSidebar from "@/components/FiltersSidebar/FiltersSidebar";
@@ -13,6 +14,7 @@ import { FormikProps } from "formik";
 
 export default function CampersCatalog() {
   const [appliedFilters, setAppliedFilters] = useState<GetCampersParams>({});
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
 
   const formikRef = useRef<FormikProps<FilterValues>>(null);
 
@@ -38,11 +40,38 @@ export default function CampersCatalog() {
   return (
     <div className={css.campersPage}>
       <div className={css.filters}>
-        <FiltersSidebar
-          innerRef={formikRef}
-          onSearch={handleSearch}
-          onReset={handleResetForm}
-        />
+        <button
+          className={css.filterToggleButton}
+          onClick={() => setIsFiltersOpen(!isFiltersOpen)}
+        >
+          <span className={css.iconWrapper}>
+            <Image
+              src="/icons/filter.svg"
+              alt="filter icon"
+              width={24}
+              height={24}
+              className={`${css.icon} ${isFiltersOpen ? css.iconHidden : ""}`}
+            />
+            <Image
+              src="/icons/filter-close.svg"
+              alt="close icon"
+              width={24}
+              height={24}
+              className={`${css.icon} ${!isFiltersOpen ? css.iconHidden : ""}`}
+            />
+          </span>
+          Filters
+        </button>
+
+        <aside
+          className={`${css.sidebarWrapper} ${isFiltersOpen ? css.open : ""}`}
+        >
+          <FiltersSidebar
+            innerRef={formikRef}
+            onSearch={handleSearch}
+            onReset={handleResetForm}
+          />
+        </aside>
       </div>
       <CamperList
         filters={appliedFilters}
